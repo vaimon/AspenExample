c799aff17e2224c21fa01af0a7e27e013dfdf7db
Andrew Nikolaev <Andrew.Vaimon@yandex.ru>
2024-02-28 10:33:16 +0300

    Add filter chips & search bar

--- app/src/main/java/me/vaimon/aspenexample/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/me/vaimon/aspenexample/ui/screens/home/HomeScreen.kt
@@ -1,36 +1,37 @@
 package me.vaimon.aspenexample.ui.screens.home
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -41,7 +42,6 @@ import me.vaimon.aspenexample.navigation.NavigationDestination
 import me.vaimon.aspenexample.ui.common.CompositeHeader
 import me.vaimon.aspenexample.ui.theme.AspenExampleTheme
 import me.vaimon.aspenexample.ui.theme.Gray
-import me.vaimon.aspenexample.ui.theme.VeryLightBlue
 import me.vaimon.aspenexample.util.PreviewMediumScreen
 
 object HomeDestination : NavigationDestination {
@@ -69,12 +69,12 @@ fun HomeScreen(
 fun HomeBody(
     modifier: Modifier = Modifier
 ) {
-    var searchQuery by remember { mutableStateOf("") }
     Column(modifier.padding(vertical = 40.dp)) {
+        val padding = Modifier
+            .padding(horizontal = 20.dp)
+            .fillMaxWidth()
         Row(
-            Modifier
-                .padding(horizontal = 20.dp)
-                .fillMaxWidth(),
+            modifier = padding,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             CompositeHeader(
@@ -85,37 +85,89 @@ fun HomeBody(
             )
             CurrentLocationLabel()
         }
-        SearchBar(
-            query = searchQuery,
-            onQueryChange = { searchQuery = it },
-            onSearch = {},
-            active = false,
-            onActiveChange = {},
-            leadingIcon = {
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.icon_search),
-                    contentDescription = null,
-                    tint = Gray
-                )
-            },
-            placeholder = {
-                Text(
-                    text = stringResource(R.string.search_label),
-                    style = MaterialTheme.typography.bodySmall,
-                    color = Gray
-                )
-            },
-            shape = RoundedCornerShape(24.dp),
-            colors = SearchBarDefaults.colors(
-                containerColor = MaterialTheme.colorScheme.surface,
-            ),
-            tonalElevation = 0.dp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
+        Spacer(modifier = Modifier.height(24.dp))
+        SearchField(modifier = padding)
+        Spacer(modifier = Modifier.height(32.dp))
+        CategoryRow(
+            listOf("Location", "Hotels", "Food", "Adventure", "Accommodation")
+        )
+    }
+}
 
+@Composable
+fun CategoryRow(
+    categories: List<String>,
+    modifier: Modifier = Modifier
+) {
+    var selectedCategoryIndex by remember { mutableIntStateOf(0) }
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        contentPadding = PaddingValues(horizontal = 20.dp),
+        modifier = modifier
+    ) {
+        itemsIndexed(categories) { index, item ->
+            val isSelected = index == selectedCategoryIndex
+            FilterChip(
+                selected = isSelected,
+                onClick = { selectedCategoryIndex = index },
+                label = {
+                    Text(
+                        item,
+                        style = if (isSelected)
+                            MaterialTheme.typography.labelMedium
+                        else
+                            MaterialTheme.typography.bodySmall,
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(
+                            horizontal = 0.dp,
+                            vertical = 12.dp
+                        ),
+                    )
+                },
+                shape = RoundedCornerShape(33.dp),
+                border = null,
+                colors = FilterChipDefaults.filterChipColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    selectedContainerColor = MaterialTheme.colorScheme.surface,
+                    selectedLabelColor = MaterialTheme.colorScheme.primary
+                )
+            )
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchField(modifier: Modifier = Modifier) {
+    var searchQuery by remember { mutableStateOf("") }
+    SearchBar(
+        query = searchQuery,
+        onQueryChange = { searchQuery = it },
+        onSearch = {},
+        active = false,
+        onActiveChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.icon_search),
+                contentDescription = null,
+                tint = Gray
+            )
+        },
+        placeholder = {
+            Text(
+                text = stringResource(R.string.search_label),
+                style = MaterialTheme.typography.bodySmall,
+                color = Gray
+            )
+        },
+        shape = RoundedCornerShape(24.dp),
+        colors = SearchBarDefaults.colors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        tonalElevation = 0.dp,
+        modifier = modifier
+    ) {
+
     }
 }
 
